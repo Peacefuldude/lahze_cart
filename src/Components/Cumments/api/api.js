968ce2a0a79aa1749e8a36849daeb587f5bf7f08
amirmMRI <amirmohammadmehri14@gmail.com>
2023-11-26 11:31:12 +0330

    input

--- src/Components/Cumments/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const getServices = async () => {
-    const response = await axios.get(`https://api.vip4care.ir/addService/getServices`)
-    return response.data;
-}
-
-export { getServices };
